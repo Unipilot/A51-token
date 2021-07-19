23765fab2b880a5c1e76d406c16a4affc335a062
Mubashir-ali-baig <mubashirbaig98@gmail.com>
2021-07-19 22:15:44 +0500

    build: added permit features

--- contracts/Pilot.sol
+++ contracts/Pilot.sol
@@ -2,26 +2,32 @@
 
 pragma solidity >=0.8.6;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract Pilot is ERC20 {
+contract Pilot is ERC20Burnable {
   address public timelock;
 
   address private _minter;
+
   address public constant FOUNDATION = 0xAfA13aa8F1b1d89454369c28b0CE1811961A7907;
 
+  // keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)")
   bytes32 private constant EIP712DOMAIN_HASH =
     0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f;
 
+  // bytes32 private constant NAME_HASH = keccak256("Pilot")
   bytes32 private constant NAME_HASH =
     0xbf5bd13a20f5bf0800ee2089e53a178ed2e5611e9703c242c63c882c6ab831d6;
 
+  // bytes32 private constant VERSION_HASH = keccak256("1")
   bytes32 private constant VERSION_HASH =
     0xc89efdaa54c0f20c7adf612882df0950f5a951637e0307cdcb4c672f298b8bc6;
 
+  // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
   bytes32 public constant PERMIT_TYPEHASH =
     0x6e71edae12b1b97f4d1f60370fef10105fa2faae0126114a169c64845d6126c9;
 
+  // keccak256("TransferWithAuthorization(address from,address to,uint256 value,uint256 validAfter,uint256 validBefore,bytes32 nonce)");
   bytes32 public constant TRANSFER_WITH_AUTHORIZATION_TYPEHASH =
     0x7c7c6cdb67a18743f49ec6fa9b35f50d52ed05cbed4cc592e13b44501c1a2267;
 
@@ -41,7 +47,7 @@ contract Pilot is ERC20 {
 
   event AuthorizationUsed(address indexed authorizer, bytes32 indexed nonce);
 
-  constructor(address _timelock) ERC20("Pilot", "PILOT") {
+  constructor(address _timelock) ERC20("Unipilot", "PILOT") {
     _mint(FOUNDATION, 20000000000000000000000000);
     timelock = _timelock;
   }
@@ -49,13 +55,6 @@ contract Pilot is ERC20 {
   function mint(address to, uint256 value) external onlyMinter returns (bool) {
     _mint(to, value);
     return true;
-  }
-
-  function burnFrom(address account, uint256 amount) external {
-    uint256 currentAllowance = allowance(account, msg.sender);
-    require(currentAllowance >= amount, "ERC20: burn amount exceeds allowance");
-    _approve(account, msg.sender, currentAllowance - amount);
-    _burn(account, amount);
   }
 
   function updateMinter(address newMinter) external onlyTimelock {
